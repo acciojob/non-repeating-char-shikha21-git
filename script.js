5244154d46d5385af93bcdafed195a823770886b
shikha dhar <shikhadhar212@gmail.com>
2023-07-12 15:01:47 +0530

    submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function firstNonRepeatingChar(str)
+{
+    const charCount = new Map();
+    for(const char of str)
+    {
+        charCount.set(char, (charCount.get(char) || 0)+1);
+    }
+    for(const char of str)
+    {
+        if(charCount.get(char) === 1)
+        {
+            return char;
+        }
+    }
+    return null;
+}
+const userInput = prompt("Enter a string");
+const result = firstNonRepeatingChar(userInput);
+if(result){
+    alert(result);
+}
